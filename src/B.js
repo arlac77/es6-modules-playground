7482d954d06ec9f794129e75ca71529d888d4b1d
Markus Felten <Markus.Felten@gmx.de>
2016-09-23 19:04:26 +0200

    feat: add StateTransitionMixin

--- src/B.js
+++ src/B.js
@@ -11,12 +11,67 @@ import {
 }
 from 'loglevel-mixin';
 
-export default class B extends LogLevelMixin(A) {
+import {
+  StateTransitionMixin,
+  defineActionMethods,
+  prepareActions
+}
+from 'statetransition-mixin';
+
+
+const actions = prepareActions({
+  start: {
+    stopped: {
+      target: 'running',
+      during: 'starting',
+      rejected: 'failed',
+      timeout: 5000
+    }
+  },
+  restart: {
+    stopped: {
+      target: 'running',
+      during: 'starting',
+      rejected: 'failed',
+      timeout: 5000
+    },
+    running: {
+      target: 'running',
+      during: 'restarting',
+      timeout: 5000
+    }
+  },
+  stop: {
+    running: {
+      target: 'stopped',
+      during: 'stopping',
+      rejected: 'failed',
+      timeout: 5000
+    },
+    starting: {
+      target: 'stopped',
+      during: 'stopping',
+      rejected: 'failed',
+      timeout: 5000
+    },
+    failed: {
+      target: 'stopped',
+      during: 'stopping',
+      rejected: 'failed',
+      timeout: 1000
+    }
+  }
+});
+
+export default class B extends StateTransitionMixin(LogLevelMixin(A), actions, 'stopped') {
   constructor() {
     super();
     console.log('constructor B');
     this.type = getType('string');
     console.log(`type: ${this.type.name}`);
     console.log(`logLevel: ${this.logLevel}`);
+    console.log(`state: ${this.state}`);
   }
 }
+
+defineActionMethods(A.prototype, actions);
