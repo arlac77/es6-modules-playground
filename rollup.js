3040563250a7a95ed42a489830a49dadab7fdfff
Markus Felten <Markus.Felten@gmx.de>
2016-09-23 12:07:31 +0200

    actially working to use es2015 modules from within npm

---
+++ rollup.js
@@ -0,0 +1,8 @@
+/* jslint node: true, esnext: true */
+'use strict';
+
+import node_resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  plugins: [node_resolve()]
+};
--- src/B.js
+++ src/B.js
@@ -3,13 +3,16 @@
 
 import A from './A';
 
-import getType from 'model-attributes';
+import {
+  getType
+}
+from 'model-attributes';
 
 export default class B extends A {
   constructor() {
     super();
     console.log('constructor B');
     this.type = getType('string');
-    //console.log(`type: ${this.type}`);
+    console.log(`type: ${this.type.name}`);
   }
 }
