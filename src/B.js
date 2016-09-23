f5b61e720f90a61221d52a536502af2c8307ec13
Markus Felten <Markus.Felten@gmx.de>
2016-09-23 13:35:44 +0200

    feat: use LogLevelMixin

--- src/B.js
+++ src/B.js
@@ -2,17 +2,21 @@
 'use strict';
 
 import A from './A';
-
 import {
   getType
 }
 from 'model-attributes';
+import {
+  LogLevelMixin
+}
+from 'loglevel-mixin';
 
-export default class B extends A {
+export default class B extends LogLevelMixin(A) {
   constructor() {
     super();
     console.log('constructor B');
     this.type = getType('string');
     console.log(`type: ${this.type.name}`);
+    console.log(`logLevel: ${this.logLevel}`);
   }
 }
