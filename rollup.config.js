cd2426a6de9a3d4495bda0271242edfac5c10b8d
Markus Felten <Markus.Felten@gmx.de>
2018-09-21 21:44:49 +0200

    chore(rollup): nodeResolve -> resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,15 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
-
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
 
 export default {
-  plugins: [nodeResolve(), commonjs()],
+  plugins: [resolve(), commonjs()],
   input: pkg.module,
-
   output: {
     file: pkg.main,
-    format: 'cjs'
+    format: "cjs",
+    interop: false
   }
 };
